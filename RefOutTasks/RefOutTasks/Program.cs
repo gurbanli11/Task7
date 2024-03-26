281434af874cdf954f4159387a18daa27c285aa3
gurbanli11 <orkhan.80@bk.ru>
2024-03-27 02:55:48 +0400

    task7 upload

---
+++ RefOutTasks/RefOutTasks/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace RefOutTasks
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        // int[] nums = {-1,-2,-3};
+        //    ChangeNum(ref nums);
+        //    foreach (int i in nums)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+
+        //    string text = "Salam,men Code Academyde tehsil aliram.";
+        //    TestText(ref text);
+        //    Console.WriteLine(TestText(ref text));
+
+        //    int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //    Console.WriteLine(Min(ref num));
+
+        //    int num1 = 56789;
+        //    NumLength(ref num1);
+
+        //    string text1 = "Bu gun novruz bayramidir.";
+        //    VowelLetter(ref text1);
+
+        //    int[]num2 = { 1,2,3,4,5,6,7,8,9,10 };
+        //    Average(ref num2);
+
+        //}
+        //public static void ChangeNum(ref int[] nums)
+        //{
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i]<=0)
+        //        {
+        //            nums[i] *= 0;
+        //        }
+        //    }
+        //    return;
+        //}
+
+        //public static bool TestText(ref string text)
+        //{
+        //    for (int i = 0; i < text.Length; i++)
+        //    {
+        //        if (text[i]=='a')
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //}    
+
+        //public static int Min(ref int[] arr)
+        //{
+        //    int min = arr[0];
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i]==min)
+        //        {
+        //            min = arr[i];
+        //        }
+        //    }
+        //    return min;
+        //}
+
+        //public static void NumLength (ref int num) 
+        //{
+        //    int count = 0;
+        //    int hide;
+        //    while (num!=0)
+        //    {
+        //        hide = num % 10;
+        //        count++;
+        //        num = num / 10; 
+        //    }
+        //    Console.WriteLine(count);
+        //}
+
+
+        //public static void VowelLetter(ref string text)
+        //{
+        //    int count = 0;
+        //    string vowel = "a, e, i, o, u";
+        //    for (int i = 0; i < text.Length; i++)
+        //    {
+        //        for (int j = 0; j < vowel.Length; j++)
+        //        {
+        //            if (text[i] == vowel[j])
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //    }
+        //    Console.WriteLine(count);
+        //}
+
+        //public static int Average(ref int[] arr)
+        //{
+        //   
+        //    int count = 0;
+        //    int average = 0;
+       //     for (int i = 0; i<arr.Length; i++)
+			//{
+             
+			//}
+        //}
+    }
+}
